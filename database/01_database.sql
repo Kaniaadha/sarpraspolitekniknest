37735945b96809a63fc41dfe625d311f7261ffa4
Kaniaadha <kaniaadhagayatri@student.uns.ac.id>
2026-07-10 14:12:02 +0700

    tambah file database 001

---
+++ database/01_database.sql
@@ -0,0 +1,12 @@
+/*
+=================================================
+DATABASE SISARPRAS POLITEKNIK NEST
+File : 01_database.sql
+=================================================
+*/
+
+CREATE DATABASE IF NOT EXISTS db_sisarpras
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+USE db_sisarpras;
