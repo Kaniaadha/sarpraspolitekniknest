7d246cbf97577f4d2cbad7fabba2cf7d44ef9e61
Kaniaadha <kaniaadhagayatri@student.uns.ac.id>
2026-07-13 09:02:07 +0700

    Add master and inventory dummy data

--- database/07_dummy_data.sql
+++ database/07_dummy_data.sql
@@ -57,3 +57,35 @@ VALUES
 (2,'PS005','Lobby POLINEST',180,'Lobby utama','Aktif'),
 (2,'PS006','Area Expo Mahasiswa',200,'Area pameran','Aktif'),
 (3,'PS007','Lobby Gedung B',150,'Lobby Gedung B','Aktif');
+
+INSERT INTO kategori
+(kode_kategori, nama_kategori, deskripsi, status)
+VALUES
+('KTG001','Meja','Berbagai jenis meja','Aktif'),
+('KTG002','Kursi','Berbagai jenis kursi','Aktif'),
+('KTG003','Komputer','PC dan Desktop','Aktif'),
+('KTG004','Laptop','Laptop operasional','Aktif'),
+('KTG005','Proyektor','Perangkat proyektor','Aktif'),
+('KTG006','AC','Pendingin ruangan','Aktif'),
+('KTG007','Printer','Printer dan Scanner','Aktif'),
+('KTG008','Peralatan Dapur','Peralatan praktikum kitchen','Aktif'),
+('KTG009','Peralatan Hotel','Peralatan teaching hotel','Aktif'),
+('KTG010','Peralatan Laboratorium','Peralatan laboratorium','Aktif');
+
+INSERT INTO inventaris
+(kode_inventaris,id_kategori,id_ruangan,id_public_space,nama_barang,merk,spesifikasi,jumlah,kondisi,tahun_perolehan,sumber_perolehan,status)
+VALUES
+
+('INV001',1,1,NULL,'Meja Dosen','IKEA','Kayu Jati',5,'Baik',2023,'Pembelian','Aktif'),
+('INV002',2,1,NULL,'Kursi Dosen','Informa','Kursi Kerja',10,'Baik',2023,'Pembelian','Aktif'),
+('INV003',4,1,NULL,'Laptop Admin','Lenovo','Core i5 RAM 8GB SSD 512GB',2,'Baik',2024,'Pembelian','Aktif'),
+('INV004',7,1,NULL,'Printer Epson','Epson L3250','Printer Ink Tank',1,'Baik',2024,'Pembelian','Aktif'),
+
+('INV005',8,5,NULL,'Oven Listrik','Rinnai','Oven 60 Liter',2,'Baik',2023,'Pembelian','Aktif'),
+('INV006',8,7,NULL,'Kompor Industri','Modena','4 Tungku',4,'Baik',2024,'Pembelian','Aktif'),
+
+('INV007',9,9,NULL,'Meja Receptionist','Olympic','Meja Front Office',1,'Baik',2023,'Pembelian','Aktif'),
+('INV008',9,10,NULL,'Kasur Hotel','Comforta','Queen Size',1,'Baik',2024,'Pembelian','Aktif'),
+
+('INV009',2,NULL,1,'Bangku Parkir','Custom','Bangku Besi',8,'Baik',2022,'Pembelian','Aktif'),
+('INV010',2,NULL,5,'Sofa Lobby','Informa','Sofa 3 Dudukan',4,'Baik',2024,'Pembelian','Aktif');
